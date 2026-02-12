7bd51c0ffe599e3dcacff39b7926e98d5601ee1d
nand <nand@aiyoyo.org>
2026-02-12 16:35:29 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Based on [24xx by Jason Tocci](https://jasontocci.itch.io/24xx)
-Text is licensed under CC BY 4.0 with Attribution to Jason Tocci
-Code is licensed under GNU GPL v3
+# A Webapp For Running Rolo 24xx games
+[Play Here](https://aiyoyosoftware.github.io/24xx-Companion/)
+- Based on [24xx by Jason Tocci](https://jasontocci.itch.io/24xx)
+- Text is licensed under CC BY 4.0 with Attribution to Jason Tocci
+- Code is licensed under GNU GPL v3
--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,16 @@ npm run build
 echo "Navigating to build output..."
 cd dist
 
+echo "Cleaning up any existing git repository..."
+rm -rf .git
+
 echo "Initializing git repository..."
 git init
 git add -A
 git commit -m 'Deploy to GitHub Pages'
 
 echo "Pushing to gh-pages branch..."
-git push -f git@github.com:AiyoyoSoftware/24xx-Companion.git main:gh-pages
+git push -f https://github.com/AiyoyoSoftware/24xx-Companion.git master:gh-pages
 
 echo "Cleaning up..."
 cd ..
